f66314fd074256a676f8d3cf2f9740149b2a6707
Dirk <dirkpp74@gmail.com>
2018-11-20 09:50:34 +0000

    initial

---
+++ client/xcomponent/login-component.js
@@ -0,0 +1,21 @@
+var LoginComponent = xcomponent.create({
+    tag: 'login-component',
+    url: 'our-login.html',
+    dimensions: {
+        width: 400,
+        height: 600
+    },
+    logLevel: 'debug',
+    props: {
+        // PART 1: Complete the login-Component.js Definition file
+            // Implement the following properties
+                // email. type is string and required is false
+                // onLogin. type is function required is false
+                // getUser. type is function required is false
+                // user.  type is object required is false
+        
+
+        // END PART 1
+        
+    }
+});
