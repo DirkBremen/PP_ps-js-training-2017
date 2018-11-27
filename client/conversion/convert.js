72fab1342e2019a48759ef549f41ee9c76d6b6ec
Bremen <dbremen@paypal.com>
2018-11-27 11:18:48 +0000

    updated

--- client/conversion/convert.js
+++ client/conversion/convert.js
@@ -3,21 +3,29 @@
 
 
 // PART 1: Create script tag
-    // Use google to figure out how to create a script tag
-        // Source should be "//www.paypalobjects.com/api/checkout.js"
-        // Set the onload property to the paypalCheckoutReady function
-        // Append the script to the document head.
+// Use google to figure out how to create a script tag
+// Source should be "//www.paypalobjects.com/api/checkout.js"
+// Set the onload property to the paypalCheckoutReady function
+// Append the script to the document head.
 
+var payPalScriptTag = document.createElement('script');
+payPalScriptTag.type = 'text/javascript';
+payPalScriptTag.src = "//www.paypalobjects.com/api/checkout.js";
+payPalScriptTag.onload = paypalCheckoutReady;
+document.head.appendChild(payPalScriptTag);
 
 
 // END PART 1
 
 // PART 2: Remove the old paypal button
-    // Go to newegg.coms cart page and click the paypal button
-        // Copy the EC token from the URL and place it in a variable named paypalToken
-    // Inspect the current button
-    // Target the button and place it in a variable called oldButton
-        // use innerHTML to remove the current contents
+// Go to newegg.coms cart page and click the paypal button
+// Copy the EC token from the URL and place it in a variable named paypalToken
+// Inspect the current button
+// Target the button and place it in a variable called oldButton
+// use innerHTML to remove the current contents
+var paypalToken = '';
+var oldButton = document.getElementsByClassName('PayPalBtn')[0];
+oldButton.innerHTML = '';
 
 
 // END PART 2
@@ -30,18 +38,36 @@ function paypalCheckoutReady() {
         paypalSetup = true;
 
         // PART 3: Render the Button Component
-            // Set environment to production
-            // Create onCancel callback that redirects
-            // Create onError callback that console.logs the error
-            // Create payment callback that returns the paypalToken from above
-            // Create onAuthorize callback that redirects
-            // Style the button with the blue color
-            // Pass the oldButton variable as the DOM id
+        // Set environment to production
+        // Create onCancel callback that redirects
+        // Create onError callback that console.logs the error
+        // Create payment callback that returns the paypalToken from above
+        // Create onAuthorize callback that redirects
+        // Style the button with the blue color
+        // Pass the oldButton variable as the DOM id
 
-        
-        
-        
+        paypal.Button.render({
+            env: 'production',
+            onCancel: function (data, actions) {
+                return actions.redirect();
+            },
+            onError: function (err) {
+                console.log(err);
+            },
+            payment: function () {
+                return paypalToken;
+            },
+            onAuthorize: function (data, action) {
+                return actions.redirect();
+            },
+            style: {
+                color: 'blue'
+            }
+        }, oldButton);
+
+
+
         // END PART 3
-        
+
     }
 }
--- src/routes/paypal/classic/setexpresscheckout/index.js
+++ src/routes/paypal/classic/setexpresscheckout/index.js
@@ -1,15 +1,15 @@
 export default function (router) {
     router.get('/', (req, res) => {
-        
+
         let paypal = req.app.kraken.get('paypalClassic');
         let c9config = req.app.kraken.get('cloud9');
 
         let nvpParams = {
-            RETURNURL: 'http://localhost:3000/misc/returnurl.html',
-            CANCELURL: 'http://localhost:3000/misc/cancelurl.html',
-            PAYMENTREQUEST_0_AMT: '100.00'
+            RETURNURL: 'http://localhost:8080/misc/returnurl.html',
+            CANCELURL: 'http://localhost:8080/misc/cancelurl.html',
+            PAYMENTREQUEST_0_AMT: '10.00'
         };
-            
+
         paypal.call('SetExpressCheckout', nvpParams, (err, response) => {
             if (err) {
                 res.json(err);
